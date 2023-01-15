0708dbf34797986d77bc9d18ffb9661e601a9a22
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-01-15 10:59:49 -0500

    tidy up for new test under discent model correct

--- mkdata/03-simulation_maestro.R
+++ mkdata/03-simulation_maestro.R
@@ -16,7 +16,7 @@ simulation_df$modname[4] <- "NeVary"
 # magic numbers
 #......................
 lambdaCOI <-  readRDS("mkdata/simdata/optim_lambda.RDS")[2] #Default from verity et al for a coi 2 = 1.593624 given that the COI in the DRC: 2.23 (2.15– 2.31)
-tlim <- 10 # assume IBD to 10 generations as before from verity et al
+tlim <- 25 # assume IBD to 25 generations for recent coalescent
 mscale <- 0.5 # assume that our mix of superinfection vs coinfection is split
 nDemes <- nrow(simulation_df$migmat[[1]])
 
@@ -38,7 +38,7 @@ pos <- sort(sample(pflen, 1e3))
 simulation_df$rho <- list(rho)
 simulation_df$pos <- list(pos)
 simulation_df$m <- list(rep(mscale, nDemes))
-simulation_df$tlim <- 10
+simulation_df$tlim <- tlim
 simulation_df$mean_coi = list(rep(lambdaCOI, nDemes))
 simulation_df$demeNames <- list(colnames(simulation_df$migmat[[1]]))
 
