56a7cb1ba54934e11337eeff04acb083d9f32c36
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-05-17 20:28:31 -0400

    update mem

--- run_future.sh
+++ run_future.sh
@@ -3,12 +3,12 @@
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=nbrazeau@med.unc.edu
 #SBATCH --ntasks=36
-#SBATCH --mem=64G
+#SBATCH --mem=96G
 #SBATCH --time=3-00:00:00
 #SBATCH --output=polysim_%j.log
 
 ## Uncomment line to run each step of future
 ## Rscript analyses/_future_polySimIBD.R 2> polysim.log
-## Rscript analyses/_future_searchgrid.R 2> searchgrid.log
+Rscript analyses/_future_searchgrid.R 2> searchgrid.log
 ## Rscript analyses/_future_discent.R 2> discent.log
 
