b869d278b36ad9576e8c3ceef104ac4a47868da1
Nicholas Brazeau <nbrazeau@med.unc.edu>
2023-05-27 10:42:40 -0400

    increase time

--- run_mk_simdata.sbatch.sh
+++ run_mk_simdata.sbatch.sh
@@ -4,7 +4,7 @@
 #SBATCH --mail-user=nbrazeau@med.unc.edu
 #SBATCH --ntasks=1
 #SBATCH --mem=8G
-#SBATCH --time=3-00:00:00
+#SBATCH --time=6-00:00:00
 #SBATCH --output=%j.log
 
 ## Uncomment line to run each step of future
